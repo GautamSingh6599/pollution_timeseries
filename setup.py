726f0244d0ee1bd4b78dd98f6ce06df171e4775e
Gautam Singh <gautam6599123@gmail.com>
2024-03-02 18:47:12 +0530

    Setup environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this project is to explore the LLMs for air quality forecasting and beat existing methods on benchmark datasets.',
+    author='Gautam Singh',
+    license='MIT',
+)
